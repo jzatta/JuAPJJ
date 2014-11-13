ddabc93b29d7479f047195479294cce01c86bebe
Johnaltan Neves <johnaltan@gmail.com>
2014-11-13 21:58:40 -0200

    PlayerClass eh uma enum

--- rpg/system/PlayerClass.java
+++ rpg/system/PlayerClass.java
@@ -1,31 +1,10 @@
 package rpg.system;
 
-public class PlayerClass {
-	
-	private int classID;
-	private String className;
-	
-	public PlayerClass(int classID){
-		this.classID = classID;
-		generateClass(classID);
-		className = "Test";
-	}
-	public String toString(){
-		return classID + "\n" + className;
-	}
-	
-	public void generateClass(int id){
-		switch(id){
-		case 0:
-			this.className = "Aprendiz";
-			break;
-		case 1: 
-			this.className = "Guerreiro";
-			break;
-		case 2:
-			this.className = "Mago";
-			break;
-		}
-	}
+public enum PlayerClass {
+	Aprendiz(0), Guerreiro(1), Mago(2);
+	private final int id;
 
+	PlayerClass(int id){
+		this.id = id;
+	}
 }
