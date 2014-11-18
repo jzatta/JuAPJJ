83f398019b70651f8f5769c04c23c2eda8eca1d7
Johnaltan Neves <johnaltan@gmail.com>
2014-11-18 16:06:40 -0200

    Teste fromJson

--- src/JohnaltanTester.java
+++ src/JohnaltanTester.java
@@ -12,6 +12,8 @@ class JohnaltanTester{
 	public static void main(String[] args){
 		MyString s = new MyString("Ola");
 		Gson g = new Gson();
+		NameGenerator n = g.fromJson("{nouns:[\"Nome1\",\"Nome2\"],adjectives:[\"Adj1\",\"Adj2\"]}",NameGenerator.class);
 		System.out.println(g.toJson(s));
+		System.out.println(n.getName());
 	}
 }
