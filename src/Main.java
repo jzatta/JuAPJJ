6a517382160723c26abde7b276dc4f0ab61b7daf
Johnaltan Neves <johnaltan@gmail.com>
2014-11-25 10:54:30 -0200

    Nao inicializa GUI sempre

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
 class Main{
 	public static void main(String[] args){
-		IOManager ioManager = new GUI();
+		IOManager ioManager = null;
 		if(args.length > 0 && args[0].equals("c")) {
 			ioManager = new Console();
+		}else{
+			ioManager = new GUI();
 		}
 		new Master(ioManager).run();
 	}
