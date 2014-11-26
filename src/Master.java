28b1ce21491cd02828e23643815cc2669787daf9
Guilherme Cardoso <gchicoper@mail.com>
2014-11-25 23:25:27 -0200

    O jogo agora toca musiquinha. para colocar uma musica, apenas coloque um arquivo .wav na pasta do jogo (por exemplo, num diretorio resources) e descomente aquela linha na classe Master (MusicPlayer mPlayer = new MusicPlayer(../resources/music.wav) obviamente colocando a url do seu arquivo ali na inicialização do mPlayer

--- src/Master.java
+++ src/Master.java
@@ -82,6 +82,7 @@ class Master {
 	
 	public void run(){
 		try{
+        //    MusicPlayer mPlayer = new MusicPlayer("../resources/music.wav");
 			while(initialMenu()){
 				do{
 					Room room = scene.generateRoom(player.getLevel());
