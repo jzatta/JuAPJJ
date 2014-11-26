e9ece3a2b1561f0ac06e58287bf6f25d073ed1cd
Guilherme Cardoso <gchicoper@mail.com>
2014-11-25 23:48:26 -0200

    a musica agora toca em loop

--- src/MusicPlayer.java
+++ src/MusicPlayer.java
@@ -8,15 +8,15 @@ public class MusicPlayer{
     private DataLine.Info info;
     private Clip clip;
 
-    public MusicPlayer(String fileUrl){
+    public MusicPlayer(String url){
         try{
-            this.music = new File(fileUrl);
+            this.music = new File(url);
             stream = AudioSystem.getAudioInputStream(this.music);
             format = stream.getFormat();
             info = new DataLine.Info(Clip.class,format);
             clip = (Clip)AudioSystem.getLine(info);
             clip.open(stream);
-            clip.start();
+            clip.loop(Clip.LOOP_CONTINUOUSLY);
         }catch(Exception e){
             e.printStackTrace();
         }   
